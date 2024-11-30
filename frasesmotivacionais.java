1f95acc874560ec2cc8779794cbfeba9a871a7b7
Muri-maguinho <168602967+Muri-maguinho@users.noreply.github.com>
2024-11-30 15:24:46 -0300

    Primeiro Projeto em Java
    Bom, esse é o meu primeiro projeto em java, eu usei o chatGPT como AUXILIAR.
    Acho que só faltou uma tela de input e output melhor do que o terminal do VS code, mas infelizmente eu não sei fazer isso.

---
+++ frasesmotivacionais.java
@@ -0,0 +1,42 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class frasesmotivacionais {
+    public static void main(String[] args) {
+
+        String[] frases = {
+            "Você consegue!",
+            "Acredite no seu potencial!",
+            "Os erros te tornam mais forte",
+            "Se compare com você do passado, não com os outros.",
+            "Impossível é uma palavra que serve só de enfeite no dicionário.",
+            "Não acredite em tudo o que vê. Todo mundo está travando uma batalha interna. Não é fácil para ninguém.",
+            "Não tenho medo de aceitar que você não é mais o mesmo.",
+        };
+
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        int lastIdx = 0;
+
+        System.out.println("Bem vindo ao Gerador de Frases Motivacionais");
+
+        String perguntar = "s";
+        while (perguntar.equalsIgnoreCase("s") || perguntar.equalsIgnoreCase("sim")) {
+
+            int choosedIdx = random.nextInt(frases.length);
+            while (choosedIdx == lastIdx) {
+                choosedIdx = random.nextInt(frases.length);
+            }
+
+            System.out.println(frases[choosedIdx]);
+
+            System.out.println("Deseja ver outra frase? (s/n)");
+            perguntar = scanner.nextLine();
+            lastIdx = choosedIdx;
+        }
+
+        System.out.println("Obrigado por usar o Gerador de Frases Motivacionais");
+        scanner.close();
+    }
+}
